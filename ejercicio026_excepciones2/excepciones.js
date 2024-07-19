1fc6a5136ad401af23d980f456387903a5778fb0
cursojsgithub <cursojsgithub@gmail.com>
2024-07-19 20:51:34 +0200

    Cambios 19 julio
    19 julio

--- ejercicio026_excepciones2/excepciones.js
+++ ejercicio026_excepciones2/excepciones.js
@@ -1,18 +1,25 @@
-let paises = ["España", "Portugal", "Italia", "Eslovenia", "Jamaica"];
+const PAISES = ["España", "Portugal", "Italia", "Eslovenia", "Jamaica"];
+const VIAJEROS_MINIMOS=1;
+const VIAJEROS_MAXIMOS=4
+const DIAS_MINIMOS=5;
+const DIAS_MAXIMOS=10;
 
 function organizarViaje(pais, viajeros, dias){
-    if(!(paises.includes(pais)) || (viajeros <1 || viajeros >4) || (dias <5 || dias>10)){
-        
-        throw new Error ("Los argumentos no son validos")
+    if(!(PAISES.map(pais => pais.toUpperCase).includes(pais.toUpperCase))) {
+        throw new Error ("El pais no es correcto");
     }
-    else{
+    if(viajeros <VIAJEROS_MINIMOS || viajeros >VIAJEROS_MAXIMOS){
+        throw new Error ("El número de viajeros no es correcto");
+    }
+    if ((dias <DIAS_MINIMOS || dias>DIAS_MAXIMOS)){
+        throw new Error ("El número de días no es correcto");
+    }
     return console.log(`Se ha reservado un viaje a ${pais} de ${dias} dias para ${viajeros} personas`);
-    }
 }
 
 try {
-    organizarViaje("España", 3, 7)
+    organizarViaje("PORtugal", 3, 8);
 }
 catch (e){
-    console.error("El error es " + e)
-}
+    console.error("El error es " + e);
+};
